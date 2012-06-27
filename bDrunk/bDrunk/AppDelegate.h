b90a842803bb6c043031957e0444431d18cb9199
Andi <andi@Andis-MacBook.local>
2012-06-27 20:15:03 +0200

    -Schalter hinzugefügt

--- bDrunk/bDrunk/AppDelegate.h
+++ bDrunk/bDrunk/AppDelegate.h
@@ -12,6 +12,7 @@
 
 @property (strong, nonatomic) IBOutlet UIWindow *window;
 @property (strong, nonatomic) IBOutlet UIBarButtonItem *infobtn;
+@property (strong, nonatomic) IBOutlet UISwitch *onOffSwitch;
 
 -(IBAction) showInfo:(id) sender;
 
--- bDrunk/bDrunk/AppDelegate.m
+++ bDrunk/bDrunk/AppDelegate.m
@@ -10,7 +10,7 @@
 
 @implementation AppDelegate
 
-@synthesize window = _window, infobtn;
+@synthesize window = _window, infobtn, onOffSwitch;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
